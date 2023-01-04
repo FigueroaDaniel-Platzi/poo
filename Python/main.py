a58357ae94f8685b397bbaaa4a71359082d7338b
Daniel Figueroa <da.figuepa@gmail.com>
2023-01-04 12:44:46 -0500

    Objetos. Dando vida a nuestras clases en Java y Python class22

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola Mundo")
+    car = Car()
+    car.license = "AMS234"
+    car.diver = "Andres Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "QWE456"
+    car2.diver = "Andres Herrera"
+    print(vars(car2))
+
